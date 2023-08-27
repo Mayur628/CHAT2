727a55577ff2dc154ba5c3aeaddaf94cf696bdb2
krupasavadiya <47918857+krupasavadiya@users.noreply.github.com>
2023-08-27 11:53:00 -0400

    how you doing

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1 class="title">
+
+            hello, how you doin?
+        </h1>
+    </div>
+</template>
+
+<script setup>
+  
+</script>
+
+<style lang="scss" scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
