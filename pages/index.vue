e12ad350e18eb99b89f4a359ab004f1d60c19184
mayurPatodiya <>
2023-08-27 20:29:02 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,46 @@
-    <template>
-        <div class="container">
-            <div class="login">
-                <form @submit.prevent="Login">
-                    <div class="username">
-                        <title>Username:</title>
-                        <input type="text" v-model="input.username">
-                    </div>
-                    <div class="password">
-                        <title>Password:</title>
-                        <input type="password" v-model="input.password">
-                    </div>
-                    <input type="submit"  value="Submit">
-                </form>
-            </div>
-            {{ input.username }}
-            {{ input.password }}
-            
+<template>
+    <div class="container">
+        <div class="login">
+            <form @submit.prevent="Login">
+                <div class="username">
+                    <title>Username:</title>
+                    <input type="text" v-model="input.username">
+                </div>
+                <div class="password">
+                    <title>Password:</title>
+                    <input type="password" v-model="input.password">
+                </div>
+                <input type="submit"  value="Submit">
+            </form>
         </div>
-    </template>
+        
+    </div>
+</template>
 
-    <script setup>
-    let input = ref({
-        username:"mayur",
-        password:"somePassword"
-    })
-    function Login(){
-        if((input.value.username == 'saloni' || input.value.username == 'mayur') && input.value.password == 'somePassword' ){
-            localStorage.setItem('valid',true);
-            navigateTo('/chat');
-        }else{
-            
-            localStorage.removeItem('valid');
-        }
+<script setup>
+let input = ref({
+    username:"",
+    password:""
+})
+function Login(){
+    if((input.value.username == 'saloni' || input.value.username == 'mayur') && input.value.password == 'somePassword' ){
+        localStorage.setItem('valid',true);
+        localStorage.setItem('user',input.value.username);
+
+        navigateTo('/chat');
+    }else{
+        
+        localStorage.removeItem('valid');
     }
+}
 
-    </script>
+</script>
 
-    <style lang="scss" scoped>
-    .container{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
-    </style>
+<style lang="scss" scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
